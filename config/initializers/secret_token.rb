9f7aceeac87f1241b9a5285b49862e5fcda55d9c
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-20 21:56:12 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SongPunch::Application.config.secret_key_base = 'b96e65658dab07cc052a072e4ba3cf3ef20d577fd8c68aa2ba918e50674f04f5a90fe850b660b1666995c6cd42acc095fd22eab7d490ca5dd29991e9532022c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SongPunch::Application.config.session_store :cookie_store, key: '_Song_Punch_session'
